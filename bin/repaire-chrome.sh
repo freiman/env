bdd580021119741552324d6ef56ac90bc3eec3a9
freiman <freiman@livesport.eu>
2015-04-24 13:43:22 +0200

    pridani git-reporteru a vylepseni repaire-chrome skriptu

--- bin/repaire-chrome.sh
+++ bin/repaire-chrome.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-killall -q -9 chrome
+while true; do
+    read -p "Do you wish to killall chrome? y/n " yn
+    case $yn in
+        [Yy]* ) killall -q -9 chrome; break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
 
-if [ -e ~/.config/google-chrome ]; then
-
-	mv ~/.config/google-chrome ~/.config/google-chrome-backup
-
-fi
+while true; do
+    read -p "Do you wish to move google-chrome dir from ~/.config/google-chrome to ~/.config/google-chrome-backup? y/n " yn
+    case $yn in
+        [Yy]* ) [ -e ~/.config/google-chrome ] && mv ~/.config/google-chrome ~/.config/google-chrome-backup; break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
